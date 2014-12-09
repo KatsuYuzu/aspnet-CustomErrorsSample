59357b2de16dc66edc625ccba2937f2472708f38
KatsuYuzu <katsuyuzu@live.jp>
2014-12-10 00:41:42 +0900

    HttpStatusを返却させるサンプル

--- src/WebApplication/Controllers/HomeController.cs
+++ src/WebApplication/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace WebApplication.Controllers
 {
@@ -13,18 +9,9 @@ namespace WebApplication.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult ReturnStatus(int statusCode)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return new HttpStatusCodeResult(statusCode);
         }
     }
 }
